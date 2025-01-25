02d960d67b000e1997612ba590d239a519c777e9
k6zma <87338187+k6zma@users.noreply.github.com>
2025-01-25 23:06:49 +0300

    feat: create config loader from .env file

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -1,1 +1,76 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server  ServerConfig  `mapstructure:"SERVER"`
+	DB      DBConfig      `mapstructure:"DB"`
+	Logger  LoggerConfig  `mapstructure:"LOGGER"`
+	Metrics MetricsConfig `mapstructure:"METRICS"`
+	AuthAPI AuthAPIConfig `mapstructure:"API"`
+}
+
+type ServerConfig struct {
+	AppVersion string `mapstructure:"APP_VERSION"`
+	Port       int16  `mapstructure:"PORT"`
+	SSL        bool   `mapstructure:"SSL"`
+}
+
+type DBConfig struct {
+	Host         string `mapstructure:"POSTGRES_HOST"`
+	Port         int16  `mapstructure:"POSTGRES_PORT"`
+	User         string `mapstructure:"POSTGRES_USER"`
+	Password     string `mapstructure:"POSTGRES_PASSWORD"`
+	DataBaseName string `mapstructure:"POSTGRES_DB"`
+}
+
+type LoggerConfig struct {
+	Level string `mapstructure:"LOG_LEVEL"`
+}
+
+type MetricsConfig struct {
+	Port int16 `mapstructure:"PORT"`
+}
+
+type AuthAPIConfig struct {
+	APIKey string `mapstructure:"AUTH_KEY"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+
+	// Default values for app server configuration
+	viper.SetDefault("SERVER.SERVER_PORT", 8080)
+	viper.SetDefault("SERVER.SERVER_SSL", false)
+
+	// Default values for database configuration
+	viper.SetDefault("DB.POSTGRES_HOST", "localhost")
+	viper.SetDefault("DB.POSTGRES_PORT", 5432)
+	viper.SetDefault("DB.POSTGRES_USER", "admin")
+	viper.SetDefault("DB.POSTGRES_PASSWORD", "admin")
+	viper.SetDefault("DB.POSTGRES_DB", "database")
+
+	// Default values for logger configuration
+	viper.SetDefault("LOGGER.LOG_LEVEL", "info")
+
+	// Default values for metrics server configuration
+	viper.SetDefault("METRICS_PORT", 9090)
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading config: %w", err)
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("error unmarshalling config: %w", err)
+	}
+
+	return &config, nil
+}
