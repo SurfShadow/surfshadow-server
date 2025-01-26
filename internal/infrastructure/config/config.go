947051fa1e1dbff41f1958406b101a2bfc880740
k6zma <87338187+k6zma@users.noreply.github.com>
2025-01-26 15:45:15 +0300

    feat: add validating of config

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -2,43 +2,43 @@ package config
 
 import (
 	"fmt"
-
+	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Server  ServerConfig  `mapstructure:"SERVER"`
-	DB      DBConfig      `mapstructure:"DB"`
-	Logger  LoggerConfig  `mapstructure:"LOGGER"`
-	Metrics MetricsConfig `mapstructure:"METRICS"`
-	AuthAPI AuthAPIConfig `mapstructure:"API"`
+	Server  ServerConfig  `mapstructure:"SERVER" validate:"required"`
+	DB      DBConfig      `mapstructure:"DB" validate:"required"`
+	Logger  LoggerConfig  `mapstructure:"LOGGER" validate:"required"`
+	Metrics MetricsConfig `mapstructure:"METRICS" validate:"required"`
+	AuthAPI AuthAPIConfig `mapstructure:"API" validate:"required"`
 }
 
 type ServerConfig struct {
-	AppVersion string `mapstructure:"APP_VERSION"`
-	Port       uint16 `mapstructure:"PORT"`
+	AppVersion string `mapstructure:"APP_VERSION" validate:"required"`
+	Port       uint16 `mapstructure:"PORT" validate:"required,gt=0"`
 	SSL        bool   `mapstructure:"SSL"`
 }
 
 type DBConfig struct {
-	Host         string `mapstructure:"POSTGRES_HOST"`
-	Port         uint16 `mapstructure:"POSTGRES_PORT"`
-	User         string `mapstructure:"POSTGRES_USER"`
-	Password     string `mapstructure:"POSTGRES_PASSWORD"`
-	DataBaseName string `mapstructure:"POSTGRES_DB"`
-	PgDriver     string `mapstructure:"POSTGRES_DRIVER"`
+	Host         string `mapstructure:"POSTGRES_HOST" validate:"required"`
+	Port         uint16 `mapstructure:"POSTGRES_PORT" validate:"required,gt=0"`
+	User         string `mapstructure:"POSTGRES_USER" validate:"required"`
+	Password     string `mapstructure:"POSTGRES_PASSWORD" validate:"required"`
+	DataBaseName string `mapstructure:"POSTGRES_DB" validate:"required"`
+	PgDriver     string `mapstructure:"POSTGRES_DRIVER" validate:"required"`
 }
 
 type LoggerConfig struct {
-	Level string `mapstructure:"LOG_LEVEL"`
+	Level string `mapstructure:"LOG_LEVEL" validate:"required,oneof=debug info warn error dpanic panic fatal"`
 }
 
 type MetricsConfig struct {
-	Port int16 `mapstructure:"PORT"`
+	Port int16 `mapstructure:"PORT" validate:"required,gt=0"`
 }
 
 type AuthAPIConfig struct {
-	APIKey string `mapstructure:"AUTH_KEY"`
+	APIKey string `mapstructure:"AUTH_KEY" validate:"required"`
 }
 
 func LoadConfig() (*Config, error) {
@@ -68,6 +68,11 @@ func LoadConfig() (*Config, error) {
 	var config Config
 	if err := viper.Unmarshal(&config); err != nil {
 		return nil, fmt.Errorf("error unmarshalling config: %w", err)
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(&config); err != nil {
+		return nil, fmt.Errorf("error validating config: %w", err)
 	}
 
 	return &config, nil
