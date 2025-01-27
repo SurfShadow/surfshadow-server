0c1177c9f762bb218d9571d36663c5165936bac3
k6zma <87338187+k6zma@users.noreply.github.com>
2025-01-27 21:37:35 +0300

    feat: add logging in db creation

--- internal/infrastructure/db/postgress.go
+++ internal/infrastructure/db/postgress.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jmoiron/sqlx"
 
 	"github.com/SurfShadow/surfshadow-server/internal/infrastructure/config"
+	"github.com/SurfShadow/surfshadow-server/pkg/logger"
 )
 
 const (
@@ -27,14 +28,27 @@ func NewPsqlDB(c *config.DBConfig) (*sqlx.DB, error) {
 		c.Password,
 	)
 
+	logger.Instance.Debugf("DSN: %s", dataSourceName)
+
+	logger.Instance.Info("Connecting to database")
+
 	db, err := sqlx.Connect(c.PgDriver, dataSourceName)
 	if err != nil {
 		return nil, err
 	}
 
+	logger.Instance.Info("Database connected successfully")
+
+	logger.Instance.Debugf("Max open connections: %d", maxOpenConns)
 	db.SetMaxOpenConns(maxOpenConns)
+
+	logger.Instance.Debugf("Connection max lifetime: %d", connMaxLifetime)
 	db.SetConnMaxLifetime(connMaxLifetime * time.Second)
+
+	logger.Instance.Debugf("Max idle connections: %d", maxIdleConns)
 	db.SetMaxIdleConns(maxIdleConns)
+
+	logger.Instance.Debugf("Connection max idle time: %d", connMaxIdleTime)
 	db.SetConnMaxIdleTime(connMaxIdleTime * time.Second)
 
 	if err := db.Ping(); err != nil {
