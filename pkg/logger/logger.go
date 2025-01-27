9309d664875d2ddc9ff8217061e772112b6da64c
k6zma <87338187+k6zma@users.noreply.github.com>
2025-01-27 22:08:06 +0300

    feat: add singletone pattern for the logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"os"
+	"sync"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -14,47 +15,51 @@ type Logger struct {
 }
 
 var Instance *Logger
+var once sync.Once
 
 func NewLogger(cfg *config.LoggerConfig) error {
-	var zapLevel zapcore.Level
-	if err := zapLevel.UnmarshalText([]byte(cfg.Level)); err != nil {
-		return err
-	}
+	var err error
+	once.Do(func() {
+		var zapLevel zapcore.Level
+		if err = zapLevel.UnmarshalText([]byte(cfg.Level)); err != nil {
+			return
+		}
 
-	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:          "time",
-		LevelKey:         "level",
-		NameKey:          "logger",
-		CallerKey:        "caller",
-		MessageKey:       "msg",
-		EncodeTime:       zapcore.ISO8601TimeEncoder,
-		EncodeLevel:      customColorLevelEncoder,
-		EncodeCaller:     zapcore.ShortCallerEncoder,
-		ConsoleSeparator: " ",
-	}
+		encoderConfig := zapcore.EncoderConfig{
+			TimeKey:          "time",
+			LevelKey:         "level",
+			NameKey:          "logger",
+			CallerKey:        "caller",
+			MessageKey:       "msg",
+			EncodeTime:       zapcore.ISO8601TimeEncoder,
+			EncodeLevel:      customColorLevelEncoder,
+			EncodeCaller:     zapcore.ShortCallerEncoder,
+			ConsoleSeparator: " ",
+		}
 
-	var core zapcore.Core
-	if cfg.Environment == "prod" {
-		core = zapcore.NewCore(
-			zapcore.NewJSONEncoder(encoderConfig),
-			zapcore.AddSync(os.Stdout),
-			zap.NewAtomicLevelAt(zapLevel),
-		)
-	} else {
-		core = zapcore.NewCore(
-			zapcore.NewConsoleEncoder(encoderConfig),
-			zapcore.AddSync(os.Stdout),
-			zap.NewAtomicLevelAt(zapLevel),
-		)
-	}
+		var core zapcore.Core
+		if cfg.Environment == "prod" {
+			core = zapcore.NewCore(
+				zapcore.NewJSONEncoder(encoderConfig),
+				zapcore.AddSync(os.Stdout),
+				zap.NewAtomicLevelAt(zapLevel),
+			)
+		} else {
+			core = zapcore.NewCore(
+				zapcore.NewConsoleEncoder(encoderConfig),
+				zapcore.AddSync(os.Stdout),
+				zap.NewAtomicLevelAt(zapLevel),
+			)
+		}
 
-	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(0))
+		logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(0))
 
-	Instance = &Logger{
-		logger.Sugar(),
-	}
+		Instance = &Logger{
+			logger.Sugar(),
+		}
+	})
 
-	return nil
+	return err
 }
 
 func customColorLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
